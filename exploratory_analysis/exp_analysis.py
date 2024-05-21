75c98720e999f9b48d1322261eeb2b3923e0af59
Batuhan Hıdıroğlu <hidiroglubatuhan@gmail.com>
2024-05-21 15:35:06 +0300

    Finalize exp analysis results

--- exploratory_analysis/exp_analysis.py
+++ exploratory_analysis/exp_analysis.py
@@ -2,13 +2,19 @@ import pandas as pd
 import numpy as np
 import matplotlib.pylab as plt
 import seaborn as sns
+import json
 plt.style.use('ggplot')
 
-# correlation and single_regression function can not run at the same time
 def main():
     df = pd.read_csv('data/data_v3.csv')
-    # correlation(df)
-    single_regression(df)
+    correlation_result = correlation(df)
+    single_regression_result = single_regression(df)
+
+    with open('correlation_result.json', 'w') as f:
+        json.dump(correlation_result, f)
+    
+    with open('single_regression_result.json', 'w') as f:
+        json.dump(single_regression_result, f)
 
 def correlation(dataframe):
     df_corr = dataframe[['day_of_week_x', 'Hour_of_Day', 'Value (kWh)', 'Temp_avg', 'HDD', 'CDD']].dropna().corr()
@@ -17,15 +23,19 @@ def correlation(dataframe):
     plt.figure(figsize=(10, 8))
     sns.heatmap(df_corr, annot=True, cmap='coolwarm', vmin=-1, vmax=1)
     plt.title('Correlation Matrix')
-    plt.show()
+
+    plt.savefig('correlation_matrix.png')
+    plt.close()
+
+    return {'correlation_matrix': 'correlation_matrix.png'}
 
 def single_regression(dataframe):
     sns.pairplot(dataframe,
                  vars=['day_of_week_x', 'Hour_of_Day', 'Value (kWh)', 'Temp_avg', 'HDD', 'CDD'],
                  hue='notes')
-    plt.show()
-
-
+    plt.savefig('single_regression_graphs.png')
+    plt.close()
+    return {'single_regression_graphs': 'single_regression_graphs.png'}
 
 if __name__ == "__main__":
     main()
